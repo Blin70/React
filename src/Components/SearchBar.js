0fb8d3dd5899a6dc0edf3942363d9ec63631051d
Blin Axhirexha <blini.axhirexha@gmail.com>
2023-11-24 01:01:36 +0100

    Search Bar wo/Submit func

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,5 +1,21 @@
+import { useState } from "react";
+
 function SearchBar(){
-    return <h1>SearchBar</h1>
+    const [term, setTerm] = useState("");
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    }
+
+    const handleChange = (event) => {
+        setTerm(event.target.value)
+    }
+
+    return(
+        <form onSubmit={handleSubmit}>
+            <input value={term} type="text" onChange={handleChange}></input>
+        </form>
+    ) 
 }
 
 export default SearchBar;
