3f723140c61f7245b51f4943c8477bf2cdf6741f
Blin Axhirexha <blini.axhirexha@gmail.com>
2023-11-25 02:44:42 +0100

    Adding and implementing tailwind css

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { GoSearch } from "react-icons/go";
-import '../css/SearchBar.css'
 
 function SearchBar(){
     const [term, setTerm] = useState("");
@@ -14,10 +13,10 @@ function SearchBar(){
     }
 
     return(
-        <div className='search-bar'>
-            <form onSubmit={handleSubmit}>
-                <input value={term} type="text" onChange={handleChange}></input>
-                <button><GoSearch /></button>
+        <div className='search-bar border-[0] border-b border-solid border-b-gray-300 p-2.5'>
+            <form className="flex" onSubmit={handleSubmit}>
+                <input value={term} type="text" onChange={handleChange} className="w-60 h-8 rounded-tl-3xl rounded-bl-3xl border-r-0 border-black text-xl pl-1.5 outline-0"></input>
+                <button className="w-20 rounded-tr-3xl rounded-br-3xl border-l-0"><GoSearch /></button>
             </form> 
         </div>
     ) 
