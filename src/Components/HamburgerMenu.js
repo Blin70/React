3db2df9aef99916b8045efaad6f47a87e66c0ef2
Blin Axhirexha <blini.axhirexha@gmail.com>
2023-12-01 21:35:57 +0100

    Hamburger rounded style

--- src/Components/HamburgerMenu.js
+++ src/Components/HamburgerMenu.js
@@ -7,11 +7,11 @@ function HamburgerMenu() {
     ];
 
     const rendered_list = list.map((i)=>{
-        return <li key={i.name} className="w-full h-20 text-4xl list-none text-center cursor-pointer leading-loose hover:bg-stone-300">{i.name}</li>
+        return <li key={i.name} className="w-full h-20 text-4xl list-none text-center cursor-pointer leading-loose rounded-2xl hover:bg-stone-300">{i.name}</li>
     });
 
     return (
-        <div className="absolute w-1/4 h-5/5 bg-stone-200 top-15 right-0" >
+        <div className="absolute w-1/4 h-5/5 bg-stone-200 top-15 right-0 rounded-2xl" >
             <ul className="w-full p-0 m-0">
                 {rendered_list}
             </ul>
