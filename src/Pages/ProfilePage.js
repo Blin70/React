0469b6e64ba7499effd06af334643b91a3ca81d8
Blin Axhirexha <blini.axhirexha@gmail.com>
2024-01-07 00:13:14 +0100

    testing

--- src/Pages/ProfilePage.js
+++ src/Pages/ProfilePage.js
@@ -1,5 +1,26 @@
+import { useState } from "react";
+
 function ProfilePage() {
-    return <h1>Profile Page</h1>
+    const [user, setUser] = useState({
+        name:"",
+        username:"",
+        email:"",
+        password:""
+    });
+
+    const handleChange = (event) => {
+        setUser(prev => ({...prev, [event.target.name]:event.target.value}))
+    };
+    return(
+        <div>
+            <input onChange={handleChange} placeholder="Name" type="text" name="name" autoComplete="off"/>
+            <input onChange={handleChange} placeholder="Username" type="text" name="username" autoComplete="off"/>
+            <input onChange={handleChange} placeholder="Email" type="text" name="email" autoComplete="off"/>
+            <input onChange={handleChange} placeholder="Password" type="text" name="password" autoComplete="off"/>
+            <br/>
+            {`{name:${user.name} , username:${user.username} , email:${user.email} , password:${user.password}}`}
+        </div>
+    );
 };
 
 export default ProfilePage;
