b13fcb60f9f16ce0cf8f52305f413a6353af856b
Blin Axhirexha <blini.axhirexha@gmail.com>
2024-01-09 02:53:50 +0100

    Added Login and Logout functions using Auth0 library

--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@ import React from "react";
 import ReactDOM  from "react-dom/client";
 import App from "./App";
 import { NavigationProvider } from './context/navigation';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
 
 root.render(
-    <NavigationProvider>
-        <App />
-    </NavigationProvider>
+    <Auth0Provider  
+    domain="dev-if4uklkweeknsn5d.us.auth0.com" 
+    clientId="U2qxQnwAT167LYcF8aUvxpjVpt0rvnDw" 
+    authorizationParams={{redirect_uri: window.location.origin}}>
+        <NavigationProvider>
+            <App />
+        </NavigationProvider>
+    </Auth0Provider>
 );
